039ed97115005a34e45d30a7c1d9220b1505c267
Kazuki Takahashi <tkhsh.schoen@gmail.com>
2020-12-20 22:42:29 +0900

    add twist_to_roombactrl_conveter node

---
+++ roomba_500driver_meiji/src/twist_to_roombactrl_converter.cpp
@@ -0,0 +1,45 @@
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <roomba_500driver_meiji/RoombaCtrl.h>
+
+namespace roomba_500driver_meiji
+{
+class TwistToRoombactrlConverter
+{
+public:
+    TwistToRoombactrlConverter(void)
+    {
+        ctrl_pub_= nh_.advertise<roomba_500driver_meiji::RoombaCtrl>("roomba/control", 1);
+        cmd_vel_sub_ = nh_.subscribe("cmd_vel", 1,
+             &TwistToRoombactrlConverter::cmd_vel_callback, this, ros::TransportHints().reliable().tcpNoDelay(true));
+    }
+
+    void cmd_vel_callback(const geometry_msgs::TwistConstPtr& msg)
+    {
+        roomba_500driver_meiji::RoombaCtrl control;
+        control.mode = roomba_500driver_meiji::RoombaCtrl::DRIVE_DIRECT;
+        control.cntl.linear.x = msg->linear.x;
+        control.cntl.angular.z = msg->angular.z;
+        ctrl_pub_.publish(control);
+    }
+
+    void process(void)
+    {
+        ros::spin();
+    }
+private:
+    ros::NodeHandle nh_;
+    ros::NodeHandle local_nh_;
+    ros::Publisher ctrl_pub_;
+    ros::Subscriber cmd_vel_sub_;
+};
+
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "twist_to_roombactrl_converter");
+    roomba_500driver_meiji::TwistToRoombactrlConverter trc;
+    trc.process();
+    return 0;
+}
